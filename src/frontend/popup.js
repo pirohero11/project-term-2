584bcf78005a97b81784724465396a747a3d72d2
pirohero11 <143906825+pirohero11@users.noreply.github.com>
2024-07-16 12:22:31 +0300

    do frontend

---
+++ src/frontend/popup.js
@@ -0,0 +1,9 @@
+function openPopup() {
+  const popup = document.getElementById('pop-up');
+  popup.style.visibility = 'visible';
+}
+
+function closePopup() {
+  const popup = document.getElementById('pop-up');
+  popup.style.visibility = 'hidden';
+}
